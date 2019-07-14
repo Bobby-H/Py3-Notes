02910d879bbb24d17d155f24294501b69a78a14b
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2019-07-14 09:45:47 -0400

    go back over dunder methods

--- dunderMethods.py
+++ dunderMethods.py
@@ -60,3 +60,18 @@
 ## Color with RGB: (255, 255, 255)
 #white = red + blue + green
 #Since we defined an __add__ method for our Color class, we were able to add these objects together using the + operator.
+class Atom:
+  def __init__(self, label):
+    self.label = label
+  def __add__(self, other):
+    return Molecule([self, other])
+
+class Molecule:
+  def __init__(self, atoms):
+    if type(atoms) is list:
+	    self.atoms = atoms
+
+sodium = Atom("Na")
+chlorine = Atom("Cl")
+salt = Molecule([sodium, chlorine])
+# salt = sodium + chlorine
